3b1a59c9485549652494950e77c531a1814536c7
kylie <wlswn4630@gmail.com>
2022-11-20 22:10:01 +0900

    feat : login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 
+import {UserForm} from "../styles/userForm.styled";
+import {Button, TextField} from "@mui/material";
+
 export const Login = () => {
     return (
-        <>
-            <h1>Login Page</h1>
-        </>
+        <UserForm>
+        <div className={'doc-title'}>
+            <span>Login</span>
+        </div>
+        <article className={'user-form-article'}>
+            <div className={'user-form-wrap'}>
+                <div className={'user-form'}>
+                    <form>
+                        <TextField label="email" variant="outlined"
+                                    name={'email'} type={'email'}
+                                   required/>
+                        <TextField label="password" variant="outlined"
+                                   name={'password'} type={'password'}
+                                   required/>
+                        <Button variant={'contained'} type={"submit"}
+                                disabled={true}>
+                            Sign Up
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        </article>
+        </UserForm>
     )
 }
